146f956702ed1b2bee4e5e024a4e31a9c963e510
and312s <138966994+and312s@users.noreply.github.com>
2024-05-19 17:38:07 +0700

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,96 @@
 import streamlit as st
-import pandas as pd
+import pandas as pd
+import numpy as np
 import joblib
-from data_preprocessing import data_preprocessing, encoder_Marital_status, encoder_Scholarship_holder, encoder_Application_mode, encoder_Previous_qualification, encoder_Displaced, encoder_Debtor, encoder_Tuition_fees_up_to_date, encoder_Gender, encoder_Scholarship_holder
-from prediction import prediction
+
+encoder_Marital_status = joblib.load("/model/encoder_Marital_status.joblib")
+encoder_Application_mode = joblib.load("/model/encoder_Application_mode.joblib")
+encoder_Previous_qualification = joblib.load("/model/encoder_Previous_qualification.joblib")
+encoder_Displaced	= joblib.load("/model/encoder_Displaced.joblib")
+encoder_Debtor = joblib.load("/model/encoder_Debtor.joblib")
+encoder_Tuition_fees_up_to_date	= joblib.load("/model/encoder_Tuition_fees_up_to_date.joblib")
+encoder_Gender = joblib.load("/model/encoder_Gender.joblib")
+encoder_Scholarship_holder = joblib.load("/model/encoder_Scholarship_holder.joblib")
+scaler_Age_at_enrollment = joblib.load("/model/scaler_Age_at_enrollment.joblib")
+scaler_Admission_grade = joblib.load("/model/scaler_Admission_grade.joblib")
+scaler_Age_at_enrollment = joblib.load("/model/scaler_Age_at_enrollment.joblib")
+scaler_Curricular_units_1st_sem_approved = joblib.load("/model/scaler_Curricular_units_1st_sem_approved.joblib")
+scaler_Curricular_units_1st_sem_enrolled = joblib.load("/model/scaler_Curricular_units_1st_sem_enrolled.joblib")
+scaler_Curricular_units_1st_sem_evaluations = joblib.load("/model/scaler_Curricular_units_1st_sem_evaluations.joblib")
+scaler_Curricular_units_1st_sem_grade = joblib.load("/model/scaler_Curricular_units_1st_sem_grade.joblib")
+scaler_Curricular_units_1st_sem_without_evaluations = joblib.load("/model/scaler_Curricular_units_1st_sem_without_evaluations.joblib")
+scaler_Curricular_units_2nd_sem_approved = joblib.load("/model/scaler_Curricular_units_2nd_sem_approved.joblib")
+scaler_Curricular_units_2nd_sem_enrolled = joblib.load("/model/scaler_Curricular_units_2nd_sem_enrolled.joblib")
+scaler_Curricular_units_2nd_sem_evaluations = joblib.load("/model/scaler_Curricular_units_2nd_sem_evaluations.joblib")
+scaler_Curricular_units_2nd_sem_grade = joblib.load("/model/scaler_Curricular_units_2nd_sem_grade.joblib")
+scaler_Curricular_units_2nd_sem_without_evaluations = joblib.load("/model/scaler_Curricular_units_2nd_sem_without_evaluations.joblib")
+
+
+def data_preprocessing(data):
+    """Preprocessing data
+ 
+    Args:
+        data (Pandas DataFrame): Dataframe that contain all the data to make prediction 
+        
+    return:
+        Pandas DataFrame: Dataframe that contain all the preprocessed data
+    """
+    data = data.copy()
+    df = pd.DataFrame()
+    
+    df["Marital_status"] = encoder_Marital_status.transform(data["Marital_status"])
+    df["Application_mode"] = encoder_Application_mode.transform(data["Application_mode"])
+    df["Previous_qualification"] = encoder_Previous_qualification.transform(data["Previous_qualification"])
+    df["Displaced"] = encoder_Displaced.transform(data["Displaced"])
+    df["Debtor"] = encoder_Debtor.transform(data["Debtor"])
+    df["Tuition_fees_up_to_date"] = encoder_Tuition_fees_up_to_date.transform(data["Tuition_fees_up_to_date"])
+    df["Gender"] = encoder_Gender.transform(data["Gender"])
+    df["Scholarship_holder"] = encoder_Scholarship_holder.transform(data["Scholarship_holder"])
+    df["Age_at_enrollment"] = scaler_Age_at_enrollment.transform(np.asarray(data["Age_at_enrollment"]).reshape(-1,1))[0]
+    df["Admission_grade"] = scaler_Admission_grade.transform(np.asarray(data["Admission_grade"]).reshape(-1,1))[0]
+    df["Curricular_units_1st_sem_approved"] = scaler_Curricular_units_1st_sem_approved.transform(np.asarray(data["Curricular_units_1st_sem_approved"]).reshape(-1,1))[0]
+    df["Curricular_units_1st_sem_enrolled"] = scaler_Curricular_units_1st_sem_enrolled.transform(np.asarray(data["Curricular_units_1st_sem_enrolled"]).reshape(-1,1))[0]
+    df["Curricular_units_1st_sem_evaluations"] = scaler_Curricular_units_1st_sem_evaluations.transform(np.asarray(data["Curricular_units_1st_sem_evaluations"]).reshape(-1,1))[0]
+    df["Curricular_units_1st_sem_grade"] = scaler_Curricular_units_1st_sem_grade.transform(np.asarray(data["Curricular_units_1st_sem_grade"]).reshape(-1,1))[0]
+    df["Curricular_units_1st_sem_without_evaluations"] = scaler_Curricular_units_1st_sem_without_evaluations.transform(np.asarray(data["Curricular_units_1st_sem_without_evaluations"]).reshape(-1,1))[0]
+    df["Curricular_units_2nd_sem_approved"] = scaler_Curricular_units_2nd_sem_approved.transform(np.asarray(data["Curricular_units_2nd_sem_approved"]).reshape(-1,1))[0]
+    df["Curricular_units_2nd_sem_enrolled"] = scaler_Curricular_units_2nd_sem_enrolled.transform(np.asarray(data["Curricular_units_2nd_sem_enrolled"]).reshape(-1,1))[0]
+    df["Curricular_units_2nd_sem_evaluations"] = scaler_Curricular_units_2nd_sem_evaluations.transform(np.asarray(data["Curricular_units_2nd_sem_evaluations"]).reshape(-1,1))[0]
+    df["Curricular_units_2nd_sem_grade"] = scaler_Curricular_units_2nd_sem_grade.transform(np.asarray(data["Curricular_units_2nd_sem_grade"]).reshape(-1,1))[0]
+    df["Curricular_units_2nd_sem_without_evaluations"] = scaler_Curricular_units_2nd_sem_without_evaluations.transform(np.asarray(data["Curricular_units_2nd_sem_without_evaluations"]).reshape(-1,1))[0]
+    
+    return df
+    
+
+model = joblib.load("model/lr_model.joblib")
+result_target = joblib.load("model/encoder_target.joblib")
+
+expected_features = [
+    'Marital_status', 'Application_mode', 'Previous_qualification', 'Admission_grade', 
+    'Displaced', 'Debtor', 'Tuition_fees_up_to_date', 'Gender', 'Scholarship_holder', 
+    'Age_at_enrollment', 'Curricular_units_1st_sem_enrolled', 'Curricular_units_1st_sem_evaluations', 
+    'Curricular_units_1st_sem_approved', 'Curricular_units_1st_sem_grade', 
+    'Curricular_units_1st_sem_without_evaluations', 'Curricular_units_2nd_sem_enrolled', 
+    'Curricular_units_2nd_sem_evaluations', 'Curricular_units_2nd_sem_approved', 
+    'Curricular_units_2nd_sem_grade', 'Curricular_units_2nd_sem_without_evaluations'
+]
+
+def prediction(data):
+    """Making prediction
+
+    Args:
+        data (Pandas DataFrame): Dataframe that contains all the preprocessed data
+
+    Returns:
+        str: Prediction result (Graduate, Dropout)
+    """
+    # Reorder the columns of data to match expected_features
+    data = data[expected_features]
+
+    # Make the prediction
+    result = model.predict(data)
+    final_result = result_target.inverse_transform(result)[0]
+    return final_result
 
 st.set_page_config(layout="wide")
 
@@ -136,13 +224,13 @@ if st.session_state.active_tab == 'Tab 2':
             new_data = data_preprocessing(data=data)
             st.session_state.new_data = new_data  
             switch_tab('Tab 3')
-            st.experimental_rerun() 
+            st.rerun() 
 
 if st.session_state.active_tab == 'Tab 3':
     if 'new_data' in st.session_state:
         new_data = st.session_state.new_data
         with st.expander("View The Preprocessed Data"):
-            st.dataframe(data=new_data, width=800, height=400)
+            st.dataframe(data=new_data, width=3200, height=10)
         st.write("Result: {}".format(prediction(new_data)))
 
 
